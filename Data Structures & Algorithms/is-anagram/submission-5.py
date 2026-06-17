1c9e78348703c4166a004f7259d6796ab3e70bc3
Vasisht K Gaddale <gitvasisht@gmail.com>
2026-06-17 09:18:19 +0530

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d1={}
+        d2={}
+        for char in s: 
+            d1[char] = d1.get(char, 0) + 1 
+        for char in t: 
+            d2[char] = d2.get(char, 0) + 1
+        return (d1==d2)
+            
